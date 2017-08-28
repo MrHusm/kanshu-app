4ce7c3ff1c815f83f8d9a8db07855f6d1dac1762
hushengmeng <hushengmeng@dangdang.com>
2017-08-28 18:19:11 +0800

    user

---
+++ src/main/webapp/sql/init.sql
@@ -0,0 +1,17 @@
+DELETE FROM kanshu.`book`;
+DELETE FROM kanshu.`book_expand`;
+DELETE FROM kanshu.`pull_book`;
+DELETE FROM kanshu.`pull_chapter`;
+DELETE FROM kanshu.`pull_volume`;
+DELETE FROM kanshu.`volume`;
+
+DELETE FROM kanshu_chapter.`chapter0`;
+DELETE FROM kanshu_chapter.`chapter1`;
+DELETE FROM kanshu_chapter.`chapter2`;
+DELETE FROM kanshu_chapter.`chapter3`;
+DELETE FROM kanshu_chapter.`chapter4`;
+DELETE FROM kanshu_chapter.`chapter5`;
+DELETE FROM kanshu_chapter.`chapter6`;
+DELETE FROM kanshu_chapter.`chapter7`;
+DELETE FROM kanshu_chapter.`chapter8`;
+DELETE FROM kanshu_chapter.`chapter9`;
